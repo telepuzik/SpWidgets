75923657c9e7e3b50b9f6c3a929e31d0eecfc030
Ilya <ilya.melnikov@indusoft.ru>
2016-03-25 17:08:18 +0300

    commit

---
+++ SPSignalrApp/Helpers/AnotherStartup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR.StockTicker;
+using Microsoft.AspNet.SignalR.Transports;
+using Owin;
+
+namespace SPSignalrApp.Helpers
+{
+    public static class AnotherStartup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.MapSignalR();
+        }
+        public static void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            //GlobalHost.DependencyResolver.Register(typeof(Microsoft.AspNet.SignalR.Hubs.IAssemblyLocator), () => new Microsoft.AspNet.SignalR.StockTicker.AssemblyLocator());
+            GlobalHost.DependencyResolver.Register(typeof(IAssemblyLocator), () => new DefaultAssemblyLocator());
+        }
+    }
+
+    public class AssemblyLocator : IAssemblyLocator
+    {
+        public IList<Assembly> GetAssemblies()
+        {
+            // list your hubclass assemblies here
+            return new System.Collections.Generic.List<Assembly>(new[] { typeof(StockTickerHub).Assembly });
+        }
+    }
+}
---
+++ SPSignalrApp/Helpers/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNet.SignalR.Hubs;
+using Owin;
+
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public static class Startup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+        public static void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            GlobalHost.DependencyResolver.Register(typeof(Microsoft.AspNet.SignalR.Hubs.IAssemblyLocator), () => new AssemblyLocator());
+        }
+    }
+
+    public class AssemblyLocator : IAssemblyLocator
+    {
+        public IList<Assembly> GetAssemblies()
+        {
+            // list your hubclass assemblies here
+            return new System.Collections.Generic.List<Assembly>(new[] { typeof(StockTickerHub).Assembly});
+        }
+    }
+}
