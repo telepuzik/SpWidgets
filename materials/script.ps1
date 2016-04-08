abfc45dd64c3f83fd932d3121b03a0a2917fd19b
Ilya <ilya.melnikov@indusoft.ru>
2016-04-08 13:50:33 +0300

    commit

--- materials/script.ps1
+++ materials/script.ps1
@@ -2,4 +2,4 @@ $site = Get-SPweb "http://localhost"
 $site.MasterUrl = "/_catalogs/masterpage/custom2.master"
 $site.CustomMasterUrl = "/_catalogs/masterpage/custom2.master"
 $site.Update()
-$site.Dispose
+$site.Dispose()
